265fc0884083efede1df44111694fa46eed7f2f9
Yevgeniy <evaldman@yahoo.com>
2020-12-10 11:03:30 -0500

    commit tree 4e1b55ec08f44454c8a210c72e20dd407950b0e0

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
 
+class Book
+    def initialize(title)
+        @title = title
+    end
+    
+    def title #getter
+        @title
+    end    
+    def author=(author) #setter
+        @author = author
+    end
+    def author #getter
+        @author
+    end 
+    def page_count=(num) #setter
+        @page_count = num
+    end
+    def page_count
+        @page_count
+    end
+    def genre=(genre) #setter
+        @genre = genre
+    end
+    def genre #getter
+        @genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+ 
+end
+
+Book.new("And Then There Were None") 
+book = Book.new("Some Title")
+book.turn_page
